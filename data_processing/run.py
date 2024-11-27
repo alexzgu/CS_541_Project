6752d0bfd11d662d663004958b1fe4f738c3574f
Alexander Gu <alexanderzgu@gmail.com>
2024-11-27 06:01:13 -0500

    data prep - audio clips
    split raw audio into audio clips

--- data_processing/run.py
+++ data_processing/run.py
@@ -1,4 +1,5 @@
 from data_processing.subtitles.clean_subtitles import clean_subtitles
+from data_processing.subtitles.prepare_token_classification_data import segment_audio
 import warnings
 
 # suppress FutureWarnings
@@ -12,15 +13,25 @@ if __name__ == '__main__':
     raw_subtitles_dir = f'{data_dir}/raw/subtitles'
     raw_subtitles_path = f'{raw_subtitles_dir}/subtitle_files'
     ignore_times_path = f'{raw_subtitles_dir}/clips_to_exclude'
+
     index_file_path = f'{raw_subtitles_dir}/index.tsv'
 
-    stage_1_dir = f'{data_dir}/processed/subtitles/stage_1'
-    time_range_dir = f'{stage_1_dir}/time_ranges'
+    raw_vocals_dir = f'{data_dir}/raw/audio'
+    syllables_dir = f'{data_dir}/clean/syllables'
+    segment_index_file_path = f'{syllables_dir}/segment_index.csv'
+    syllable_vocals_dir = f'{syllables_dir}/clips'
 
-    print("Cleaning raw subtitle data...")
-    clean_subtitles(raw_subtitles_path, ignore_times_path, clean_subtitles_path, # f'{stage_1_dir}/subtitle_files',
-                    output_intermediates=True, intermediate_dir=time_range_dir)
-    print("Cleaned subtitle data.")
+    # stage_1_dir = f'{data_dir}/processed/subtitles/stage_1'
+    # time_range_dir = f'{stage_1_dir}/time_ranges'
+
+    # print("Cleaning raw subtitle data...")
+    # clean_subtitles(raw_subtitles_path, ignore_times_path, clean_subtitles_path, # f'{stage_1_dir}/subtitle_files',
+    #                 output_intermediates=True, intermediate_dir=time_range_dir)
+    # print("Cleaned subtitle data.")
 
     # print("Cleaning raw subtitle data...")
     # clean_subtitles_2(f'{stage_1_dir}/subtitle_files', time_range_dir, clean_subtitles_path)
+
+    print("Segmenting audio data...")
+    segment_audio(raw_vocals_dir, clean_subtitles_path, syllable_vocals_dir, segment_index_file_path)
+    print("Segmented audio data.")
---
+++ data_processing/subtitles/prepare_token_classification_data.py
@@ -0,0 +1,79 @@
+import os
+import pandas as pd
+from pydub import AudioSegment
+
+def segment_audio(vocal_audio_dir: str, clean_subtitle_dir: str,
+                  segmented_audio_dir: str, segment_index_file_path: str) -> int:
+    """
+    Segments the vocal audio data into smaller audio clips based on the cleaned subtitle data.
+    Each clip is indexed, and information about its file origin, start time, end time, and label (token)
+    are stored in a segment index file.
+    Args:
+        vocal_audio_dir: Where the vocal audio data is stored.
+        clean_subtitle_dir: Where the cleaned subtitle data is stored.
+        segmented_audio_dir: Where the segmented audio data will be stored.
+            Any existing data with the same file name will be overwritten.
+        segment_index_file_path:  Where the file containing indices for segments will be stored.
+            If it already exists, the data will be appended to the existing file.
+
+    Returns: 0 if successful, -1 otherwise.
+    """
+    segment_index_file = os.path.join(segment_index_file_path)
+
+    # create segmented audio directory if it doesn't exist
+    # if not os.path.exists(segmented_audio_dir):
+    #     os.makedirs(segmented_audio_dir)
+
+    # write initial header to segment index file
+    # Index, File, Start, End, Token
+    with open(segment_index_file, 'w') as f:
+        f.write("Index,File,Start,End,Token\n")
+
+    # iterates through files in the clean_subtitle_data_path
+    for file in os.listdir(clean_subtitle_dir):
+        if file.endswith(".csv"):
+            print(f"Segmenting audio for file: {file}")
+            subtitles_file = os.path.join(clean_subtitle_dir, file)
+            vocal_audio_file = os.path.join(vocal_audio_dir, f"{file[:-4]}.mp3")
+            segment_audio_file(subtitles_file, vocal_audio_file, segmented_audio_dir, segment_index_file)
+    return 0
+
+
+def segment_audio_file(subtitles_file: str, vocal_audio_file: str, segmented_audio_dir: str, segment_index_file: str):
+    """
+    Segments the vocal audio data into smaller audio clips based on the cleaned subtitle data.
+    Args:
+        subtitles_file:
+        vocal_audio_file:
+        segmented_audio_dir:
+        segment_index_file:
+
+    Returns:
+
+    """
+    file_idx = subtitles_file.split('/')[-1].split('.')[0]
+
+    # read in the subtitle data
+    df = pd.read_csv(subtitles_file)
+
+    # read in the vocal audio data
+    vocal_audio = AudioSegment.from_file(vocal_audio_file)
+
+    # filter for rows where exclude = True
+    df = df[df['exclude'] == False].reset_index(drop=True)
+    for idx, row in df.iterrows():
+        NUM_SEGMENTS_PER_SECOND = 1000
+        start = row['start'] * NUM_SEGMENTS_PER_SECOND
+        end = row['end']  * NUM_SEGMENTS_PER_SECOND
+        token = row['token']
+
+        # segment the audio
+        segment = vocal_audio[start:end]
+
+        # save the segment
+        segmented_audio_file = os.path.join(segmented_audio_dir, f"{file_idx}_{idx}.mp3")
+        segment.export(segmented_audio_file, format="mp3")
+
+        # append entry to segment index file
+        with open(segment_index_file, 'a') as f:
+            f.write(f"{file_idx}_{idx},{segmented_audio_file},{start},{end},{token}\n")
