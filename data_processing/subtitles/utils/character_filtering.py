fca813dd871cb8eca5de3d4207427e8921086865
Alexander Gu <alexanderzgu@gmail.com>
2024-11-26 20:12:06 -0500

    Update character_filtering.py
    updated to reflect more precise ranges for determining whether a character is hiragana, katakana, or neither

--- data_processing/subtitles/utils/character_filtering.py
+++ data_processing/subtitles/utils/character_filtering.py
@@ -1,14 +1,46 @@
+def is_hira(char: str) -> bool:
+    """
+    Determines if a character is hiragana.
+    Args:
+        char: Character to check.
+    Returns: Whether the character is hiragana.
+    """
+    return '\u3041' <= char <= '\u3094'
+
+
+def is_kata(char: str) -> bool:
+    """
+    Determines if a character is katakana.
+    Args:
+        char: Character to check.
+    Returns: Whether the character is katakana.
+    """
+    return '\u30A1' <= char <= '\u30F4'
+
+
+def is_long_vowel(char: str) -> bool:
+    """
+    Determines if a character is a long vowel sound.
+    Args:
+        char: Character to check.
+    Returns: Whether the character is a long vowel sound.
+    """
+    return char == '\u30FC'
+
+
 def to_kana(token: str, include_katakana: bool = False) -> str:
     """
     Filters out non-Japanese characters from a token.
     Args:
         token: Token to filter.
-        include_katakana:  Whether to include katakana characters.
-    Returns: Filtered token; by default, this includes just hiragana.
+        include_katakana:  Whether to also include katakana characters.
+    Returns: Filtered token; by default, this includes just hiragana and the long vowel sound.
     """
+
     if include_katakana:
-        return ''.join([char for char in token if '\u3040' <= char <= '\u30FF'])
-    return ''.join([char for char in token if '\u3040' <= char <= '\u309F'])
+        return ''.join([char for char in token if is_hira(char) or is_kata(char) or is_long_vowel(char)])
+    return ''.join([char for char in token if is_hira(char) or is_long_vowel(char)])
+
 
 def to_alphanumeric(token: str) -> str:
     """
@@ -19,6 +51,12 @@ def to_alphanumeric(token: str) -> str:
     """
     return ''.join([char for char in token if char.isalnum()])
 
-def placeholder():
-    # TODO: do something about ・and ー.
-    pass
+
+def kana_to_hira(token: str) -> str:
+    """
+    Converts katakana to hiragana.
+    Args:
+        token: Token to convert.
+    Returns: Converted token.
+    """
+    return ''.join([chr(ord(char) - 96) if is_kata(char) else char for char in token])
