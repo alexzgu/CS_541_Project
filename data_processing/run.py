d8fc1af571d17a95e1144881f05ad995dd53ddf0
Alexander Gu <alexanderzgu@gmail.com>
2024-12-06 22:53:53 -0500

    data change
    - implemented silence reduction
    - segment breaks in 10 ms

--- data_processing/run.py
+++ data_processing/run.py
@@ -4,13 +4,17 @@ from data_processing.main_functions.clean_subtitles import clean_subtitles
 from data_processing.main_functions.change_last_end_to_vid_length import change_end_for_directory
 from data_processing.prepare_segment_break_data import find_segments_breaks
 from data_processing.prepare_token_classification_data import segment_audio, reindex_audio_segments
+from data_processing.utils.reduce_silence import reduce_silence_for_directory
 
 # suppress FutureWarnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 if __name__ == '__main__':
+    # paths
     data_dir = '../data'
+
     clean_subtitles_path = f'{data_dir}/clean/subtitles'
+    processed_subtitles_path = f'{data_dir}/processed/subtitles'
     tokens_file_path = f'{data_dir}/config/tokens.txt'
 
     raw_subtitles_dir = f'{data_dir}/raw/subtitles'
@@ -28,19 +32,17 @@ if __name__ == '__main__':
     syllable_vocals_dir = f'{syllables_dir}/clips'
     segment_break_dir = f'{data_dir}/clean/segment_breaks'
 
-    # stage_1_dir = f'{data_dir}/processed/subtitles/stage_1'
-    # time_range_dir = f'{stage_1_dir}/time_ranges'
-
+    # do things
     # print("Cleaning raw subtitle data...")
-    # clean_subtitles(raw_subtitles_path, ignore_times_path, clean_subtitles_path, tokens_file_path)
+    # clean_subtitles(raw_subtitles_path, ignore_times_path, processed_subtitles_path, tokens_file_path)
     # print("Cleaned subtitle data.")
     #
     # print("Segmenting audio data...")
-    # segment_audio(clean_vocals_dir, clean_subtitles_path, syllable_vocals_dir,
+    # segment_audio(clean_vocals_dir, processed_subtitles_path, syllable_vocals_dir,
     #               segment_index_file_path, padding=50)
     # print("Segmented audio data.")
     #
-    # # must run indexing right after segmenting
+    # # # must run indexing right after segmenting
     # print("Indexing audio data...")
     # reindex_audio_segments(segment_index_file_path,
     #                        segment_index_file_path,
@@ -48,11 +50,16 @@ if __name__ == '__main__':
     # print("Indexed audio data.")
     #
     # print("Changing last end to vid length...")
-    # change_end_for_directory(clean_subtitles_path, raw_vocals_dir, segment_index_file_path)
+    # change_end_for_directory(processed_subtitles_path, raw_vocals_dir, segment_index_file_path)
     # print("Changed last end to vid length.")
 
-    # for segment break detection
+    # reduce silence
+    print("Reducing silence...")
+    reduce_silence_for_directory(processed_subtitles_path, clean_vocals_dir,
+                                 clean_subtitles_path)
+    print("Reduced silence.")
 
+    # for segment break detection
     print("Finding segment breaks...")
-    find_segments_breaks(clean_subtitles_path, segment_break_dir, sample_length=50)
+    find_segments_breaks(clean_subtitles_path, segment_break_dir, sample_length=10)
     print("Found segment breaks.")
